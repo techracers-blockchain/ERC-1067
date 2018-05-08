cd8c6b89612946dcd5937a93347b59dd9c23bc81
Nidish Ramakrishnan <nramakrishnan@grepruby.com>
2018-05-08 12:52:37 +0530

    Fine tuned DataCentre for ERC specific tasks

--- contracts/DataCentre.sol
+++ contracts/DataCentre.sol
@@ -6,8 +6,6 @@ import "zeppelin-solidity/contracts/ownership/Ownable.sol";
 contract DataCentre is Ownable {
     struct Container {
         mapping(bytes32 => uint256) values;
-        mapping(bytes32 => address) addresses;
-        mapping(bytes32 => bool) switches;
         mapping(address => uint256) balances;
         mapping(address => mapping (address => uint)) constraints;
     }
@@ -17,14 +15,6 @@ contract DataCentre is Ownable {
     // Owner Functions
     function setValue(bytes32 _container, bytes32 _key, uint256 _value) public onlyOwner {
         containers[_container].values[_key] = _value;
-    }
-
-    function setAddress(bytes32 _container, bytes32 _key, address _value) public onlyOwner {
-        containers[_container].addresses[_key] = _value;
-    }
-
-    function setBool(bytes32 _container, bytes32 _key, bool _value) public onlyOwner {
-        containers[_container].switches[_key] = _value;
     }
 
     function setBalanace(bytes32 _container, address _key, uint256 _value) public onlyOwner {
@@ -38,14 +28,6 @@ contract DataCentre is Ownable {
     // Constant Functions
     function getValue(bytes32 _container, bytes32 _key) public constant returns(uint256) {
         return containers[_container].values[_key];
-    }
-
-    function getAddress(bytes32 _container, bytes32 _key) public constant returns(address) {
-        return containers[_container].addresses[_key];
-    }
-
-    function getBool(bytes32 _container, bytes32 _key) public constant returns(bool) {
-        return containers[_container].switches[_key];
     }
 
     function getBalanace(bytes32 _container, address _key) public constant returns(uint256) {
